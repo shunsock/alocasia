1d44c79d69c3232af678ed66df4285972b543d23
Shun <s.tsuchiya.business@gmail.com>
2025-01-12 22:33:15 +0900

    mbstringの拡張を使うことを明示

--- shell.nix
+++ shell.nix
@@ -2,14 +2,17 @@
 
 pkgs.mkShell {
   buildInputs = [
-    # PHP 8.3
-    pkgs.php83
+    # PHP 8.3 with mbstring enabled
+    (pkgs.php83.withExtensions (extensions: with extensions.all; [ mbstring ]))
     # Composer
     pkgs.php83.packages.composer
+
     # PHPStan
     pkgs.php83.packages.phpstan
+
     # PHPUnit
     pkgs.phpunit
+
     # Justfile
     pkgs.just
   ];
