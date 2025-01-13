4a35a6860137326a530b3ed2ce0cf12afbb988a7
Shun <s.tsuchiya.business@gmail.com>
2025-01-13 17:00:16 +0900

    dep: PHPUnitの依存を追加しました

--- shell.nix
+++ shell.nix
@@ -3,15 +3,12 @@
 pkgs.mkShell {
   buildInputs = [
     # PHP 8.3 with mbstring enabled
-    (pkgs.php83.withExtensions (extensions: with extensions.all; [ mbstring ctype ]))
+    (pkgs.php83.withExtensions (extensions: with extensions.all; [ mbstring ctype dom tokenizer xmlwriter ]))
     # Composer
     pkgs.php83.packages.composer
 
     # PHPStan
     pkgs.php83.packages.phpstan
-
-    # PHPUnit
-    pkgs.phpunit
 
     # Justfile
     pkgs.just
