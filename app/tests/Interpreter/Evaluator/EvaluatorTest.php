8b7f5c69d7e8b85335d41a45b8dd6abec360fbd5
Shun <s.tsuchiya.business@gmail.com>
2025-01-26 15:03:32 +0900

    四則演算のインテグレーションテスト

--- app/tests/Interpreter/Evaluator/EvaluatorTest.php
+++ app/tests/Interpreter/Evaluator/EvaluatorTest.php
@@ -9,7 +9,12 @@ use Alocasia\Interpreter\Evaluator\EvaluatorException;
 use Alocasia\Interpreter\Evaluator\StackedItem\AlocasiaBlock\AlocasiaBlock;
 use Alocasia\Interpreter\Evaluator\StackedItem\AlocasiaObject\AlocasiaObject;
 use Alocasia\Interpreter\Evaluator\StackedItem\AlocasiaObject\AlocasiaObjectType;
+use Alocasia\Interpreter\Token\Asterisk;
+use Alocasia\Interpreter\Token\Block;
+use Alocasia\Interpreter\Token\FloatLiteral;
 use Alocasia\Interpreter\Token\IntegerLiteral;
+use Alocasia\Interpreter\Token\Plus;
+use Alocasia\Interpreter\Token\Slash;
 use PHPUnit\Framework\TestCase;
 
 class EvaluatorTest extends TestCase
@@ -41,5 +46,63 @@ class EvaluatorTest extends TestCase
         $this->assertInstanceOf(AlocasiaObject::class, $evaluator->stack[0]);
         $this->assertEquals(AlocasiaObjectType::INTEGER, $evaluator->stack[0]->type);
         $this->assertEquals(1, $evaluator->stack[0]->value);
+    }
+
+    /**
+     * @throws EvaluatorException
+     */
+    public function testEvaluateArithmeticOperation(): void
+    {
+        // Setup
+        // 1 2 + { 4 2 / } * 2 => 6.0
+        $tokens = [
+            new IntegerLiteral(
+                line: 1,
+                position: 1,
+                value: 1,
+            ),
+            new FloatLiteral(
+                line: 2,
+                position: 2,
+                value: 2.0,
+            ),
+            new Plus(line: 1, position: 3),
+            // 1 + 2.0 = 3.0
+            new Block(
+                line: 1,
+                position: 4,
+                tokens: [
+                    new IntegerLiteral(
+                        line: 1,
+                        position: 5,
+                        value: 4,
+                    ),
+                    new IntegerLiteral(
+                        line: 1,
+                        position: 6,
+                        value: 2,
+                    ),
+                    new Slash(
+                        line: 1,
+                        position: 7
+                    )
+                ]
+            ),
+            // 4 / 2 = 2
+            new Asterisk(
+                line: 1,
+                position: 8,
+            )
+            // 3.0 * 2 = 6.0
+        ];
+        $evaluator = new Evaluator(hashmap: [], stack: [], tokens: $tokens);
+
+        // Run
+        $evaluator->evaluate();
+
+        // Assert
+        $this->assertInstanceOf(AlocasiaObject::class, $evaluator->stack[0]);
+        $this->assertEquals(AlocasiaObjectType::FLOAT, $evaluator->stack[0]->type);
+        $this->assertEquals(6.0, $evaluator->stack[0]->value);
     }
 }
