d8ecb37cecaa63077eae253b7edc97bd333fb2c0
Shun <s.tsuchiya.business@gmail.com>
2025-02-03 01:41:30 +0900

    remove comment out

--- flake.nix
+++ flake.nix
@@ -11,15 +11,12 @@
       let
         pkgs = import nixpkgs { inherit system; };
 
-        # PHP 8.3 に必要な拡張を追加したパッケージを定義
         phpWithExtensions = pkgs.php83.withExtensions (extensions: with extensions.all; [ mbstring ctype dom tokenizer xmlwriter pcntl ]);
 
-        # vendor を生成するための derivation
         alocasiaDerivation = pkgs.stdenv.mkDerivation {
           pname = "alocasia";
           version = "1.0.0";
           src = self;
-          # buildInputs に、composer と PHP (拡張付き) を指定
           buildInputs = [ pkgs.php83.packages.composer phpWithExtensions ];
           buildPhase = ''
             cd app
@@ -31,7 +28,6 @@
           '';
         };
 
-        # 実行用のラッパースクリプトを作成
         appScript = pkgs.writeShellScriptBin "alocasia" ''
           exec ${phpWithExtensions}/bin/php ${alocasiaDerivation}/app/src/main.php "$@"
         '';
