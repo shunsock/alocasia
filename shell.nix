e01cd681d07fa07bfb8f311b1c8d1a1ad02450a8
Shun <s.tsuchiya.business@gmail.com>
2025-02-08 00:26:06 +0900

    fix ci

--- shell.nix
+++ shell.nix
@@ -6,12 +6,6 @@ pkgs.mkShell {
     (pkgs.php83.withExtensions (extensions: with extensions.all; [ mbstring ctype dom tokenizer xmlwriter ]))
     # Composer
     pkgs.php83.packages.composer
-
-    # PHPStan
-    pkgs.php83.packages.phpstan
-
-    # Justfile
-    pkgs.just
   ];
 }
 
